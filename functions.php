07fb16cf895f1c264e678585bd2db034e16fddc3
Kenneth <kennethgeenen@gmail.com>
2012-02-03 22:41:50 +0100

    theme added

---
+++ functions.php
@@ -0,0 +1,296 @@
+<?php
+/*
+Author: Eddie Machado
+URL: htp://themble.com/bones/
+
+This is where you can drop your custom functions or
+just edit things like thumbnail sizes, header images, 
+sidebars, comments, ect.
+*/
+
+// Get Bones Core Up & Running!
+require_once('library/bones.php');            // core functions (don't remove)
+require_once('library/plugins.php');          // plugins & extra functions (optional)
+require_once('library/custom-post-type.php'); // custom post type example
+
+// Options panel
+require_once('library/options-panel.php');
+
+// Shortcodes
+require_once('library/shortcodes.php');
+
+// Admin Functions (commented out by default)
+// require_once('library/admin.php');         // custom admin functions
+
+// Custom Backend Footer
+function bones_custom_admin_footer() {
+	echo '<span id="footer-thankyou">Developed by <a href="http://320press.com" target="_blank">320press</a></span>. Built using <a href="http://themble.com/bones" target="_blank">Bones</a>.';
+}
+
+// adding it to the admin area
+add_filter('admin_footer_text', 'bones_custom_admin_footer');
+
+// Set content width
+if ( ! isset( $content_width ) ) $content_width = 580;
+
+/************* THUMBNAIL SIZE OPTIONS *************/
+
+// Thumbnail sizes
+add_image_size( 'wpbs-featured', 580, 300, true );
+add_image_size( 'bones-thumb-600', 600, 150, false );
+add_image_size( 'bones-thumb-300', 300, 100, true );
+/* 
+to add more sizes, simply copy a line from above 
+and change the dimensions & name. As long as you
+upload a "featured image" as large as the biggest
+set width or height, all the other sizes will be
+auto-cropped.
+
+To call a different size, simply change the text
+inside the thumbnail function.
+
+For example, to call the 300 x 300 sized image, 
+we would use the function:
+<?php the_post_thumbnail( 'bones-thumb-300' ); ?>
+for the 600 x 100 image:
+<?php the_post_thumbnail( 'bones-thumb-600' ); ?>
+
+You can change the names and dimensions to whatever
+you like. Enjoy!
+*/
+
+/************* ACTIVE SIDEBARS ********************/
+
+// Sidebars & Widgetizes Areas
+function bones_register_sidebars() {
+    register_sidebar(array(
+    	'id' => 'sidebar1',
+    	'name' => 'Main Sidebar',
+    	'description' => 'Used on every page BUT the homepage page template.',
+    	'before_widget' => '<div id="%1$s" class="widget %2$s">',
+    	'after_widget' => '</div>',
+    	'before_title' => '<h4 class="widgettitle">',
+    	'after_title' => '</h4>',
+    ));
+    
+    register_sidebar(array(
+    	'id' => 'sidebar2',
+    	'name' => 'Homepage Sidebar',
+    	'description' => 'Used only on the homepage page template.',
+    	'before_widget' => '<div id="%1$s" class="widget %2$s">',
+    	'after_widget' => '</div>',
+    	'before_title' => '<h4 class="widgettitle">',
+    	'after_title' => '</h4>',
+    ));
+    
+    /* 
+    to add more sidebars or widgetized areas, just copy
+    and edit the above sidebar code. In order to call 
+    your new sidebar just use the following code:
+    
+    Just change the name to whatever your new
+    sidebar's id is, for example:
+    
+    
+    
+    To call the sidebar in your template, you can just copy
+    the sidebar.php file and rename it to your sidebar's name.
+    So using the above example, it would be:
+    sidebar-sidebar2.php
+    
+    */
+} // don't remove this bracket!
+
+/************* COMMENT LAYOUT *********************/
+		
+// Comment Layout
+function bones_comments($comment, $args, $depth) {
+   $GLOBALS['comment'] = $comment; ?>
+	<li <?php comment_class(); ?>>
+		<article id="comment-<?php comment_ID(); ?>" class="clearfix">
+			<div class="comment-author vcard row clearfix">
+				<div class="avatar span2">
+					<?php echo get_avatar($comment,$size='75',$default='<path_to_url>' ); ?>
+				</div>
+				<div class="span8">
+					<?php printf(__('<h4 class="span8">%s</h4>'), get_comment_author_link()) ?>
+					<?php edit_comment_link(__('Edit'),'<span class="edit-comment btn small info">','</span>') ?>
+                    
+                    <?php if ($comment->comment_approved == '0') : ?>
+       					<div class="alert-message success">
+          				<p><?php _e('Your comment is awaiting moderation.') ?></p>
+          				</div>
+					<?php endif; ?>
+                    
+                    <?php comment_text() ?>
+                    
+                    <time datetime="<?php echo comment_time('Y-m-j'); ?>"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php comment_time('F jS, Y'); ?> </a></time>
+                    
+                    <!-- removing reply link on each comment since we're not nesting them -->
+					<?php //comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
+                </div>
+			</div>
+		</article>
+    <!-- </li> is added by wordpress automatically -->
+<?php
+} // don't remove this bracket!
+
+/************* SEARCH FORM LAYOUT *****************/
+
+// Search Form
+function bones_wpsearch($form) {
+    $form = '<form role="search" method="get" id="searchform" action="' . home_url( '/' ) . '" >
+    <label class="screen-reader-text" for="s">' . __('Search for:', 'bonestheme') . '</label>
+    <input type="text" value="' . get_search_query() . '" name="s" id="s" placeholder="Search the Site..." />
+    <input type="submit" id="searchsubmit" value="'. esc_attr__('Search') .'" />
+    </form>';
+    return $form;
+} // don't remove this bracket!
+
+/****************** password protected post form *****/
+
+add_filter( 'the_password_form', 'custom_password_form' );
+
+function custom_password_form() {
+	global $post;
+	$label = 'pwbox-'.( empty( $post->ID ) ? rand() : $post->ID );
+	$o = '<div class="clearfix"><form class="protected-post-form" action="' . get_option('siteurl') . '/wp-pass.php" method="post">
+	' . __( "<p>This post is password protected. To view it please enter your password below:</p>" ) . '
+	<label for="' . $label . '">' . __( "Password:" ) . ' </label><div class="input"><input name="post_password" id="' . $label . '" type="password" size="20" /><input type="submit" name="Submit" class="btn primary" value="' . esc_attr__( "Submit" ) . '" /></div>
+	</form></div>
+	';
+	return $o;
+}
+
+/*********** update standard wp tag cloud widget so it looks better ************/
+
+add_filter( 'widget_tag_cloud_args', 'my_widget_tag_cloud_args' );
+
+function my_widget_tag_cloud_args( $args ) {
+	$args['number'] = 20; // show less tags
+	$args['largest'] = 9.75; // make largest and smallest the same - i don't like the varying font-size look
+	$args['smallest'] = 9.75;
+	$args['unit'] = 'px';
+	return $args;
+}
+
+add_filter('wp_tag_cloud','wp_tag_cloud_filter', 10, 2);
+
+function wp_tag_cloud_filter($return, $args)
+{
+  return '<div id="tag-cloud">'.$return.'</div>';
+}
+
+// Enable shortcodes in widgets
+add_filter('widget_text', 'do_shortcode');
+
+// Disable jump in 'read more' link
+function remove_more_jump_link($link) {
+	$offset = strpos($link, '#more-');
+	if ($offset) {
+		$end = strpos($link, '"',$offset);
+	}
+	if ($end) {
+		$link = substr_replace($link, '', $offset, $end-$offset);
+	}
+	return $link;
+}
+add_filter('the_content_more_link', 'remove_more_jump_link');
+
+// Remove height/width attributes on images so they can be responsive
+add_filter( 'post_thumbnail_html', 'remove_thumbnail_dimensions', 10 );
+add_filter( 'image_send_to_editor', 'remove_thumbnail_dimensions', 10 );
+
+function remove_thumbnail_dimensions( $html ) {
+    $html = preg_replace( '/(width|height)=\"\d*\"\s/', "", $html );
+    return $html;
+}
+
+// Add the Meta Box to the homepage template
+function add_homepage_meta_box() {  
+    add_meta_box(  
+        'homepage_meta_box', // $id  
+        'Custom Fields', // $title  
+        'show_homepage_meta_box', // $callback  
+        'page', // $page  
+        'normal', // $context  
+        'high'); // $priority  
+}  
+add_action('add_meta_boxes', 'add_homepage_meta_box');
+
+// Field Array  
+$prefix = 'custom_';  
+$custom_meta_fields = array(  
+    array(  
+        'label'=> 'Homepage tagline area',  
+        'desc'  => 'Displayed underneath page title. Only used on homepage template. HTML can be used.',  
+        'id'    => $prefix.'tagline',  
+        'type'  => 'textarea' 
+    )  
+);  
+
+// The Homepage Meta Box Callback  
+function show_homepage_meta_box() {  
+global $custom_meta_fields, $post;  
+// Use nonce for verification  
+echo '<input type="hidden" name="custom_meta_box_nonce" value="'.wp_create_nonce(basename(__FILE__)).'" />';  
+  
+    // Begin the field table and loop  
+    echo '<table class="form-table">';  
+    foreach ($custom_meta_fields as $field) {  
+        // get value of this field if it exists for this post  
+        $meta = get_post_meta($post->ID, $field['id'], true);  
+        // begin a table row with  
+        echo '<tr> 
+                <th><label for="'.$field['id'].'">'.$field['label'].'</label></th> 
+                <td>';  
+                switch($field['type']) {  
+                    // text  
+                    case 'text':  
+                        echo '<input type="text" name="'.$field['id'].'" id="'.$field['id'].'" value="'.$meta.'" size="60" /> 
+                            <br /><span class="description">'.$field['desc'].'</span>';  
+                    break;
+                    
+                    // textarea  
+                    case 'textarea':  
+                        echo '<textarea name="'.$field['id'].'" id="'.$field['id'].'" cols="80" rows="4">'.$meta.'</textarea> 
+                            <br /><span class="description">'.$field['desc'].'</span>';  
+                    break;  
+                } //end switch  
+        echo '</td></tr>';  
+    } // end foreach  
+    echo '</table>'; // end table  
+}  
+
+// Save the Data  
+function save_homepage_meta($post_id) {  
+    global $custom_meta_fields;  
+  
+    // verify nonce  
+    if (!wp_verify_nonce($_POST['custom_meta_box_nonce'], basename(__FILE__)))  
+        return $post_id;  
+    // check autosave  
+    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)  
+        return $post_id;  
+    // check permissions  
+    if ('page' == $_POST['post_type']) {  
+        if (!current_user_can('edit_page', $post_id))  
+            return $post_id;  
+        } elseif (!current_user_can('edit_post', $post_id)) {  
+            return $post_id;  
+    }  
+  
+    // loop through fields and save the data  
+    foreach ($custom_meta_fields as $field) {  
+        $old = get_post_meta($post_id, $field['id'], true);  
+        $new = $_POST[$field['id']];  
+        if ($new && $new != $old) {  
+            update_post_meta($post_id, $field['id'], $new);  
+        } elseif ('' == $new && $old) {  
+            delete_post_meta($post_id, $field['id'], $old);  
+        }  
+    } // end foreach  
+}  
+add_action('save_post', 'save_homepage_meta');  
+
+?>
---
+++ library/js/scripts.js
@@ -0,0 +1,293 @@
+/* ==========================================================
+ * bootstrap-alerts.js v1.4.0
+ * http://twitter.github.com/bootstrap/javascript.html#alerts
+ * ==========================================================
+ * Copyright 2011 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================================================== */
+
+
+!function( $ ){
+
+  "use strict"
+
+  /* CSS TRANSITION SUPPORT (https://gist.github.com/373874)
+   * ======================================================= */
+
+   var transitionEnd
+
+   $(document).ready(function () {
+
+     $.support.transition = (function () {
+       var thisBody = document.body || document.documentElement
+         , thisStyle = thisBody.style
+         , support = thisStyle.transition !== undefined || thisStyle.WebkitTransition !== undefined || thisStyle.MozTransition !== undefined || thisStyle.MsTransition !== undefined || thisStyle.OTransition !== undefined
+       return support
+     })()
+
+     // set CSS transition event type
+     if ( $.support.transition ) {
+       transitionEnd = "TransitionEnd"
+       if ( $.browser.webkit ) {
+        transitionEnd = "webkitTransitionEnd"
+       } else if ( $.browser.mozilla ) {
+        transitionEnd = "transitionend"
+       } else if ( $.browser.opera ) {
+        transitionEnd = "oTransitionEnd"
+       }
+     }
+
+   })
+
+ /* ALERT CLASS DEFINITION
+  * ====================== */
+
+  var Alert = function ( content, options ) {
+    this.settings = $.extend({}, $.fn.alert.defaults, options)
+    this.$element = $(content)
+      .delegate(this.settings.selector, 'click', this.close)
+  }
+
+  Alert.prototype = {
+
+    close: function (e) {
+      var $element = $(this).parent('.alert-message')
+
+      e && e.preventDefault()
+      $element.removeClass('in')
+
+      function removeElement () {
+        $element.remove()
+      }
+
+      $.support.transition && $element.hasClass('fade') ?
+        $element.bind(transitionEnd, removeElement) :
+        removeElement()
+    }
+
+  }
+
+
+ /* ALERT PLUGIN DEFINITION
+  * ======================= */
+
+  $.fn.alert = function ( options ) {
+
+    if ( options === true ) {
+      return this.data('alert')
+    }
+
+    return this.each(function () {
+      var $this = $(this)
+
+      if ( typeof options == 'string' ) {
+        return $this.data('alert')[options]()
+      }
+
+      $(this).data('alert', new Alert( this, options ))
+
+    })
+  }
+
+  $.fn.alert.defaults = {
+    selector: '.close'
+  }
+
+  $(document).ready(function () {
+    new Alert($('body'), {
+      selector: '.alert-message[data-alert] .close'
+    })
+  })
+
+}( window.jQuery || window.ender );
+
+/*
+Bones Scripts File
+Author: Eddie Machado
+
+This file should contain any js scripts you want to add to the site.
+Instead of calling it in the header or throwing it inside wp-head()
+this file will be called automatically in the footer so as not to 
+slow the page load.
+
+*/
+
+// Modernizr.load loading the right scripts only if you need them
+Modernizr.load([
+	{
+    // Let's see if we need to load selectivizr
+    test : Modernizr.borderradius,
+    // Modernizr.load loads selectivizr for IE6-8
+    nope : ['selectivizr-min.js']
+	}
+]);
+
+/* ============================================================
+ * bootstrap-dropdown.js v1.4.0
+ * http://twitter.github.com/bootstrap/javascript.html#dropdown
+ * ============================================================
+ * Copyright 2011 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============================================================ */
+
+
+!function( $ ){
+
+  "use strict"
+
+  /* DROPDOWN PLUGIN DEFINITION
+   * ========================== */
+
+  $.fn.dropdown = function ( selector ) {
+    return this.each(function () {
+      $(this).delegate(selector || d, 'click', function (e) {
+        var li = $(this).parent('li')
+          , isActive = li.hasClass('open')
+
+        clearMenus()
+        !isActive && li.toggleClass('open')
+        return false
+      })
+    })
+  }
+
+  /* APPLY TO STANDARD DROPDOWN ELEMENTS
+   * =================================== */
+
+  var d = 'a.menu, .dropdown-toggle'
+
+  function clearMenus() {
+    $(d).parent('li').removeClass('open')
+  }
+
+  $(function () {
+    $('html').bind("click", clearMenus)
+    $('body').dropdown( '[data-dropdown] a.menu, [data-dropdown] .dropdown-toggle' )
+  })
+
+}( window.jQuery || window.ender );
+
+/* imgsizer (flexible images for fluid sites) */
+var imgSizer={Config:{imgCache:[],spacer:"/path/to/your/spacer.gif"},collate:function(aScope){var isOldIE=(document.all&&!window.opera&&!window.XDomainRequest)?1:0;if(isOldIE&&document.getElementsByTagName){var c=imgSizer;var imgCache=c.Config.imgCache;var images=(aScope&&aScope.length)?aScope:document.getElementsByTagName("img");for(var i=0;i<images.length;i++){images[i].origWidth=images[i].offsetWidth;images[i].origHeight=images[i].offsetHeight;imgCache.push(images[i]);c.ieAlpha(images[i]);images[i].style.width="100%";}
+if(imgCache.length){c.resize(function(){for(var i=0;i<imgCache.length;i++){var ratio=(imgCache[i].offsetWidth/imgCache[i].origWidth);imgCache[i].style.height=(imgCache[i].origHeight*ratio)+"px";}});}}},ieAlpha:function(img){var c=imgSizer;if(img.oldSrc){img.src=img.oldSrc;}
+var src=img.src;img.style.width=img.offsetWidth+"px";img.style.height=img.offsetHeight+"px";img.style.filter="progid:DXImageTransform.Microsoft.AlphaImageLoader(src='"+src+"', sizingMethod='scale')"
+img.oldSrc=src;img.src=c.Config.spacer;},resize:function(func){var oldonresize=window.onresize;if(typeof window.onresize!='function'){window.onresize=func;}else{window.onresize=function(){if(oldonresize){oldonresize();}
+func();}}}}
+
+// add twitter bootstrap classes and color based on how many times tag is used
+function addTwitterBSClass(thisObj) {
+  var title = $(thisObj).attr('title');
+  if (title) {
+    var titles = title.split(' ');
+    if (titles[0]) {
+      var num = parseInt(titles[0]);
+      if (num > 0)
+      	$(thisObj).addClass('label');
+      if (num == 2)
+        $(thisObj).addClass('label notice');
+      if (num > 2 && num < 4)
+        $(thisObj).addClass('label success');
+      if (num >= 5 && num < 10)
+        $(thisObj).addClass('label warning');
+      if (num >=10)
+        $(thisObj).addClass('label important');
+    }
+  }
+  else
+  	$(thisObj).addClass('label');
+  return true;
+}
+
+// as the page loads, cal these scripts
+$(document).ready(function() {
+
+	// modify tag cloud links to match up with twitter bootstrap
+	$("#tag-cloud a").each(function() {
+	    addTwitterBSClass(this);
+	    return true;
+	});
+	
+	$("p.tags a").each(function() {
+		addTwitterBSClass(this);
+		return true;
+	});
+	
+	$("ol.commentlist a.comment-reply-link").each(function() {
+		$(this).addClass('btn success small');
+		return true;
+	});
+	
+	var $window_w = $(window).width();
+	if ($window_w > 480){
+		$('a.dropdown-toggle').css('display', 'none')
+		$('#menu-top-nav').removeClass('dropdown-menu');
+		$('#menu-top-nav').addClass('nav');
+	}
+	
+	// adjust top nav menu based on screen width
+	$(window).resize(function() {
+		var $window_w = $(window).width();
+		if ($window_w > 480){
+			$('a.dropdown-toggle').css('display', 'none')
+			$('#menu-top-nav').removeClass('dropdown-menu');
+			$('#menu-top-nav').addClass('nav');
+		}
+		else{
+			$('a.dropdown-toggle').css('display', 'inline-block')
+			$('#menu-top-nav').removeClass('nav');
+			$('#menu-top-nav').addClass('dropdown-menu');
+		}
+	});
+	
+	// Input placeholder text fix for IE
+	$('[placeholder]').focus(function() {
+	  var input = $(this);
+	  if (input.val() == input.attr('placeholder')) {
+		input.val('');
+		input.removeClass('placeholder');
+	  }
+	}).blur(function() {
+	  var input = $(this);
+	  if (input.val() == '' || input.val() == input.attr('placeholder')) {
+		input.addClass('placeholder');
+		input.val(input.attr('placeholder'));
+	  }
+	}).blur();
+	
+	// Prevent submission of empty form
+	$('[placeholder]').parents('form').submit(function() {
+	  $(this).find('[placeholder]').each(function() {
+		var input = $(this);
+		if (input.val() == input.attr('placeholder')) {
+		  input.val('');
+		}
+	  })
+	});
+	
+	$('ul.nav').dropdown();
+	
+	$('.alert-message').alert();
+ 
+}); /* end of as page load scripts */
